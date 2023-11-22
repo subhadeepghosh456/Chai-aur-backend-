1327f9e0991e00d7aead7396035e7d5dff683b2b
subhadeep <srsubhadeepghosh456@gmail.com>
2023-11-22 21:33:19 +0530

    user router

--- src/app.js
+++ src/app.js
@@ -16,3 +16,8 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 export { app };
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
