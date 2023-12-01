b655cdb732e4b9de20d0d93dd6e37c11f211f25a
subhadeep <srsubhadeepghosh456@gmail.com>
2023-12-01 21:53:26 +0530

    bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -22,7 +22,16 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req?.files?.avatar[0]?.path;
-  const coverImageLocalPath = req?.files?.coverImage[0]?.path;
+  //const coverImageLocalPath = req?.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -32,7 +41,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
-    throw new ApiError(400, "Avatar file is required");
+    throw new ApiError(400, "Avatar file is required for cloudinary");
   }
 
   const user = await User.create({
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,16 +8,34 @@ cloudinary.config({
   api_secret: process.env.api_secret,
 });
 
+// const uploadOnCloudinary = async (localFilePath) => {
+//   console.log("cloudinary file", localFilePath);
+//   try {
+//     if (!localFilePath) return null;
+//     const response = await cloudinary.v2.uploader.upload(localFilePath, {
+//       resource_type: "auto",
+//     });
+//     log("cloudinary file", response);
+//     return response;
+//   } catch (error) {
+//     fs.unlinkSync(localFilePath);
+//     return null;
+//   }
+// };
+
 const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
-    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    log(response.url);
+    // file has been uploaded successfull
+    //console.log("file is uploaded on cloudinary ", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
-    fs.unlinkSync(localFilePath);
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
     return null;
   }
 };
